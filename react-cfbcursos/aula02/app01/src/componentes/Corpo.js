6a97d423e6e5e35e62606a91f7c497281500ec83
Samuel Jacobsen Barcellos <rcbsjbarcellos@gmail.com>
2024-07-06 14:34:49 -0300

    Aula 06

--- react-cfbcursos/aula02/app01/src/componentes/Corpo.js
+++ react-cfbcursos/aula02/app01/src/componentes/Corpo.js
@@ -1,11 +1,22 @@
 import React from "react";
+import Dados from "./Dados";
 
 export default function Corpo(){
+
+    const cnl = 'CFB Cursos';
+    const yt = 'cfbcursos.youtube';
+    const crs = 'Curso de React.js';
+
     return(
         <section>
             <h2>Curso de React</h2>
             <p>Se inscreva em nosso canal</p>
             <p>Estudando React</p>
+            <Dados 
+                canal={cnl}
+                youtube={yt}
+                curso={crs}
+            />
         </section>
     );
 };
