c2abdd5d33ed78c50afba15341b6d9882c3c91d4
Samuel Jacobsen Barcellos <rcbsjbarcellos@gmail.com>
2024-07-06 09:10:10 -0300

    Aula 05

--- react-cfbcursos/aula02/app01/src/App.js
+++ react-cfbcursos/aula02/app01/src/App.js
@@ -1,22 +1,12 @@
 import React from "react";
-import Logo from './componentes/imgs/logo.png';
+import Header from "./componentes/Header.js";
+import Corpo from "./componentes/Corpo.js";
 
 export default function App(){
-  const canal=()=>{
-    return 'CFB cursos';
-  };
-  const curso = 'Curso de React';
-
   return(
-    <section>
-      <header>
-        <p>Canal: {canal()}</p>
-        <p>{curso}</p>
-      </header>
-      <section>
-        <img src={Logo}/>
-        <img src="./imgs/logo.png"/>
-      </section>
-    </section>
+    <>
+      <Header/>
+      <Corpo/>
+    </>
   );
 };
