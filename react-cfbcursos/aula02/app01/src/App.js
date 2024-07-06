375d9d294b0fb621d2758cc2d7f88894d595691f
Samuel Jacobsen Barcellos <rcbsjbarcellos@gmail.com>
2024-07-06 17:04:51 -0300

    Aula 08

--- react-cfbcursos/aula02/app01/src/App.js
+++ react-cfbcursos/aula02/app01/src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
-import Header from "./componentes/Header.js";
-import Corpo from "./componentes/Corpo.js";
+import './css/App.css';
 
 export default function App(){
+
+  const textoDestaque = {
+    color: 'blue',
+    fontSize: '3em',
+    textDecoration: 'underline'
+  };
+
   return(
     <>
-      <Header/>
-      <Corpo/>
+      <section className="caixa">
+        <h1 style={{color: 'red',fontSize: '5em'}}>CFB Cursos</h1>
+        <h2 style={textoDestaque}>Curso de React</h2>
+        <p className="texto">Aprendendo React!!!</p>
+        <a href="#">Link</a>
+      </section>
     </>
   );
 };
