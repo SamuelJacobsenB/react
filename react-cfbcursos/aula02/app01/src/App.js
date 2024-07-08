ec5b86d3f97ca22dabcbfcd70f281346d36f05c2
Samuel Jacobsen Barcellos <rcbsjbarcellos@gmail.com>
2024-07-08 14:11:29 -0300

    Aula 14

--- react-cfbcursos/aula02/app01/src/App.js
+++ react-cfbcursos/aula02/app01/src/App.js
@@ -1,33 +1,20 @@
-import React from "react";
+import React,{useState} from "react";
 import './css/App.css';
 
 export default function App(){
 
-  const carros = [
-    {categoria: 'Utilitário', preco: '100000.00', modelo: 'Hillux'},
-    {categoria: 'Esportivo', preco: '180000.00', modelo: 'Golf GTI'},
-    {categoria: 'Utilitário', preco: '80000.00', modelo: 'RAM'},
-    {categoria: 'SUV', preco: '75000.00', modelo: 'T-Cross'},
-    {categoria: 'Esportivo', preco: '359000.00', modelo: 'Camaro'},
-    {categoria: 'Utilitário', preco: '55000.00', modelo: 'Fiat-Uno'}
-  ];
-
-  const listaCarros = carros.map( 
-    (el, i)=>
-       <li>
-        <h3>Modelo: {el.modelo}</h3>
-        <p>Categoria: {el.categoria}</p>
-        <strong>Preço: {el.preco}</strong>
-        <hr/>
-       </li> 
-  );
+  const [carro, setCarro] = useState('HRV');
 
   return(
     <>
-      <h1>Carros:</h1>
-      <ul>
-        {listaCarros}
-      </ul>
+      <label>Selecione um carro: </label>
+      <select value={carro} onChange={(e)=>setCarro(e.target.value)}>
+        <option value='HRV'>HRV</option>
+        <option value='Golf'>Golf</option>
+        <option value='RAM'>RAM</option>
+        <option value='HB20'>HB20</option>
+      </select>
+      <p>Carro selecionado: {carro}</p>
     </>
   );
 };
