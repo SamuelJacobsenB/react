c80fc6d3123c03ed4fddb6ac3a43821f88de2c2a
Samuel Jacobsen Barcellos <rcbsjbarcellos@gmail.com>
2024-07-09 12:50:29 -0300

    Aula 15

--- react-cfbcursos/aula02/app01/src/App.js
+++ react-cfbcursos/aula02/app01/src/App.js
@@ -3,18 +3,28 @@ import './css/App.css';
 
 export default function App(){
 
-  const [carro, setCarro] = useState('HRV');
+  const [form, setForm] = useState({'nome': '','curso': '','ano': ''});
+  const handleFormChange=(el)=>{
+    if(el.target.getAttribute('name')=='fnome'){
+      setForm({'nome':el.target.value,'curso':form.curso,'ano':form.ano})
+    } else if(el.target.getAttribute('name')=='fcurso'){
+      setForm({'nome':form.nome,'curso':el.target.value,'ano':form.ano})
+    } else {
+      setForm({'nome':form.nome,'curso':form.curso,'ano':el.target.value})
+    }
+  };
 
   return(
     <>
-      <label>Selecione um carro: </label>
-      <select value={carro} onChange={(e)=>setCarro(e.target.value)}>
-        <option value='HRV'>HRV</option>
-        <option value='Golf'>Golf</option>
-        <option value='RAM'>RAM</option>
-        <option value='HB20'>HB20</option>
-      </select>
-      <p>Carro selecionado: {carro}</p>
+      <label>Nome</label>
+      <input type="text" name="fnome" value={form.nome} onChange={(el)=>handleFormChange(el)}/><br/>
+      <label>Curso</label>
+      <input type="text" name="fcurso" value={form.curso} onChange={(el)=>handleFormChange(el)}/><br/>
+      <label>Ano</label>
+      <input type="text" name="fano" value={form.ano} onChange={(el)=>handleFormChange(el)}/>
+      <p>Nome digitado: {form.nome}</p>
+      <p>Curso digitado: {form.curso}</p>
+      <p>Ano digitado: {form.ano}</p>
     </>
   );
 };
