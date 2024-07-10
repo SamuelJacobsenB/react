dc2057524a5338c4e06217b3bebddc1d32f79c08
Samuel Jacobsen Barcellos <rcbsjbarcellos@gmail.com>
2024-07-10 12:50:04 -0300

    Aula20

--- react-cfbcursos/aula02/app01/src/App.js
+++ react-cfbcursos/aula02/app01/src/App.js
@@ -1,22 +1,30 @@
 import React from "react";
-import { useState, useEffect } from 'react';
+import { useState } from "react";
 import './css/App.css';
-
-
 
 export default function App(){
 
-  const [contagem, setContagem] = useState(0);
+  const [nome,setNome] = useState();
 
-  useEffect(()=>{
-    console.log('Página carregada');
-    document.title = `Contagem: ${contagem}`;
-  });
+  const armazenar=(chave,valor)=>{
+    localStorage.setItem(chave,valor);
+  };
+
+  const consultar=(chave)=>{
+    alert(localStorage.getItem(chave));
+  };
+
+  const apagar=(chave)=>{
+    localStorage.removeItem(chave);
+  };
 
   return(
-    <>
-      <p>Contagem: {contagem}</p>
-      <button onClick={()=>setContagem(contagem+1)}>Contar</button>
+    <>  
+      <label>Digite um nome:</label><br/>
+      <input type="text" value={nome} onChange={(el)=>setNome(el.target.value)}/><br/>
+      <button onClick={()=>armazenar('ls_nome',nome)}>Gravar nome</button>
+      <button onClick={()=>consultar('ls_nome')}>Consultar</button>
+      <button onClick={()=>apagar('ls_nome')}>Remover nome</button>
     </>
   );
 };
